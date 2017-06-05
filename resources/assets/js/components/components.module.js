0d8662c59b82d8b3a007c40ffe40159cf171dbd2
Pavel6778 <pavelsvintsitsky@mail.ru>
2017-06-05 18:07:18 +0300

    my component

--- resources/assets/js/components/components.module.js
+++ resources/assets/js/components/components.module.js
@@ -3,10 +3,13 @@ import 'components/main/main.module';
 import 'components/layouts/layouts.module';
 import 'components/projects/projects.module';
 import 'components/users/users.module';
+import 'components/my/my.module'
 
 angular.module('app.components', [
     'app.components.layouts',
     'app.components.main',
     'app.components.projects',
     'app.components.users'
+    'app.components.projects',
+    'app.components.my',
 ]);
