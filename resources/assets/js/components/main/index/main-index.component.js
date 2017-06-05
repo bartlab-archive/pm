8721a2de83c5160b83325e6c82e66517425e713f
toliyanich <toliyanich@yandex.ru>
2017-06-05 17:14:24 +0300

    nav menu fix

--- resources/assets/js/components/main/index/main-index.component.js
+++ resources/assets/js/components/main/index/main-index.component.js
@@ -1,6 +1,6 @@
 import MainIndexController from './main-index.controller';
 import MainIndexTemplate from './main-index.html';
-
+import './main-index.scss';
 const mainIndexComponent = {
     controller: MainIndexController,
     template: MainIndexTemplate
