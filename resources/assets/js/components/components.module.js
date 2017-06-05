662ade2fc1d0c33933e48fb9c922ed7339354176
B@RT <bartlab@gmail.com>
2017-06-05 18:01:31 +0300

    admin base module

--- resources/assets/js/components/components.module.js
+++ resources/assets/js/components/components.module.js
@@ -3,10 +3,12 @@ import 'components/main/main.module';
 import 'components/layouts/layouts.module';
 import 'components/projects/projects.module';
 import 'components/users/users.module';
+import 'components/admin/admin.module';
 
 angular.module('app.components', [
     'app.components.layouts',
     'app.components.main',
     'app.components.projects',
-    'app.components.users'
+    'app.components.users',
+    'app.components.admin'
 ]);
