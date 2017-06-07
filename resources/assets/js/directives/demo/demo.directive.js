d40855a0f61278113525331a31b400e95591f555
B@RT <bartlab@gmail.com>
2017-06-07 18:43:36 +0300

    base directive class and demo directive

---
+++ resources/assets/js/directives/demo/demo.directive.js
@@ -0,0 +1,19 @@
+import DirectiveBase from 'base/directive.base';
+import demoTemplate from './demo.html';
+import './demo.scss';
+
+export default class DemoDirective extends DirectiveBase {
+
+    static get selector() {
+        return 'demo';
+    }
+
+    $onInit() {
+        this.restrict = 'EA';
+        this.template = demoTemplate;
+    }
+
+    link(scope, element, attrs, controller, transcludeFn) {
+    }
+
+}
