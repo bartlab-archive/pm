9681a49fa37760db15a7052c6d047fb11d87c747
toliyanich <toliyanich@yandex.ru>
2017-06-08 19:39:36 +0300

    project  sidemenu

--- resources/assets/js/components/layouts/default/layout-default.controller.js
+++ resources/assets/js/components/layouts/default/layout-default.controller.js
@@ -7,35 +7,35 @@ import ControllerBase from 'base/controller.base';
 export default class LayoutDefaultController extends ControllerBase {
 
     static get $inject() {
-        return ['$mdSidenav', '$state'];
+        return ['$mdSidenav', '$state', 'ProjectsService'];
     }
 
     $onInit() {
         this.items = [
             {url: 'home', name: 'Home', icon: 'home'},
             {url: 'admin.index', name: 'Administration', icon: 'apps'},
-            {
-                url: 'projects.list', name: 'Projects', icon: 'work', children: {
-                items: [
-                    {url: 'home', name: 'project1', icon: 'apps'},
-                    {url: 'admin.index', name: 'project2', icon: 'apps'},
-                    {url: 'home', name: 'project3', icon: 'apps'},
-                ],
-                shown: false
-            }
-            },
             {url: 'home', name: 'My page', icon: 'person'},
             {url: 'home', name: 'Help', icon: 'help'}
         ];
+
+        this.ProjectsService.getList()
+            .then(response => this.projects = response.data)
+            .catch(console.log);
+
     }
 
-    toggle() {
-        this.$mdSidenav('left').toggle();
+    toggle(menu = 'left') {
+        this.$mdSidenav(menu).toggle();
     }
 
     openUserMenu($mdMenu, ev) {
         $mdMenu.open(ev);
     };
+
+    gotToProject(id) {
+        this.$state.go('projects.info', {id: id});
+        this.toggle('right');
+    }
 
     myAccount() {
         this.$state.go('my.account');
@@ -48,6 +48,7 @@ export default class LayoutDefaultController extends ControllerBase {
     menuClick(item) {
         if (item.hasOwnProperty('children')) {
             item.children.shown = !item.children.shown;
+            console.log(this.projects);
         } else {
             this.$state.go(item.url);
             this.toggle();
