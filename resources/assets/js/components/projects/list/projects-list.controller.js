b33b66cf3c4ac3db78fbbd9043d4e515a30cff7a
B@RT <bartlab@gmail.com>
2017-06-12 20:46:53 +0300

    project list

--- resources/assets/js/components/projects/list/projects-list.controller.js
+++ resources/assets/js/components/projects/list/projects-list.controller.js
@@ -11,7 +11,7 @@ export default class ProjectsListController extends ControllerBase {
     }
 
     $onInit() {
-        // this.showClosed = 0;
+        this.showClosed = 1;
         this.load();
     }
 
