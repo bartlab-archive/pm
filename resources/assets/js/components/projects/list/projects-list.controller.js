bd021174826b1d76d74268320376d611902a36dd
B@RT <bartlab@gmail.com>
2017-06-13 15:16:58 +0300

    project list, layout margin fix, remove old scss

--- resources/assets/js/components/projects/list/projects-list.controller.js
+++ resources/assets/js/components/projects/list/projects-list.controller.js
@@ -11,12 +11,12 @@ export default class ProjectsListController extends ControllerBase {
     }
 
     $onInit() {
-        this.showClosed = 1;
+        this.showClosed = 0;
         this.load();
     }
 
     load() {
-        this.list = [];
+        // this.list = [];
         this.ProjectsService.getList({closed: this.showClosed}).then((response) => {
             this.list = response.data;
         });
@@ -24,6 +24,10 @@ export default class ProjectsListController extends ControllerBase {
 
     goto(identifier) {
         this.$state.go('projects-inner.info', {id: identifier});
+    }
+
+    newProject(){
+        this.$state.go('projects.new');
     }
 
 }
