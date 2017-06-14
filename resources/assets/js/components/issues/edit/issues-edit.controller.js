f905393b6b80b6b0b380111b489ed8c825762919
ArtyomSidun <tomas1772@gmail.com>
2017-06-14 18:17:50 +0300

    Edit issue page

--- resources/assets/js/components/issues/edit/issues-edit.controller.js
+++ resources/assets/js/components/issues/edit/issues-edit.controller.js
@@ -10,7 +10,15 @@ export default class IssuesEditController extends ControllerBase{
         const _this = this;
         this.IssuesService.one(this.$stateParams.id).then((response) => {
             _this.issue = response.data;
+            _this.issue.statusText = this.statusText(_this.issue.status_id);
         });
     }
 
+    statusText(id){
+        switch (id) {
+            case 5:
+                return 'Closed';
+                break;
+        }
+    }
 }
