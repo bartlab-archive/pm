080eeb32ce65bbbe4fbb21c995bf3c59cab9bf28
Pavel6778 <pavelsvintsitsky@mail.ru>
2017-06-22 18:10:26 +0300

    Wiki service

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -1,110 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\WikiRequest;
+use App\Services\WikiService;
 
-use App\Models\Project;
-use App\Models\WikiPage;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
-class WikiController extends Controller
+class WikiController
 {
-    protected function getWikiPageMarkDown($identifier, $page_title = null)
+    protected $WikiService;
+
+    function __construct(WikiService $wikiService)
     {
-        $user_projects = Auth::user()->projects;
-
-        $project = $user_projects->where('identifier', $identifier)->first();
-
-        if (is_null($project)) {
-            abort(403);
-        }
-
-        $wiki_content = $project->wiki
-            ->page()
-            ->with('content');
-
-        if ($page_title) {
-            $wiki_content->where('title', $page_title);
-        } else {
-            $wiki_content->where('parent_id', null);
-        }
-
-        $wiki_content = $wiki_content->first()->toArray();
-
-        return response()->json(array_merge(is_null($wiki_content['content']) ? [] : $wiki_content['content'] , ['title' => $wiki_content['title']]));
+        $this->WikiService = $wikiService;
     }
 
-    protected function setWikiPageMarkDown(Request $request, $project_identifier, $wiki_id, $name = null)
+    public function getWikiPageMarkDown($identifier, $page_title = null)
     {
-        $user_projects = Auth::user()->projects;
-        $project = $user_projects->where('identifier', $project_identifier)->first();
-
-        if (is_null($project)) {
-            abort(403);
-        }
-
-        $wiki_content = $project->wiki->page();
-
-        if ($name) {
-            $wiki_content->where('title', $wiki_id);
-            $wiki_id = $name;
-        } else {
-            $wiki_content->where('id', $wiki_id);
-        }
-
-        $wiki_content = $wiki_content
-            ->with(['content' => function ($q) use($wiki_id) {
-                $q->where('id', $wiki_id);
-            }])
-            ->firstOrFail();
-        $wiki_content->content->update([
-            'text' => $request->input('text')
-        ]);
-
-        $wiki_content = $wiki_content->toArray();
-
-        return response()->json(array_merge($wiki_content['content'], ['title' => $wiki_content['title']]));
+        return $this->WikiService->getWikiPageMarkDown($identifier, $page_title);
     }
 
-    protected function addNewWiki(Request $request, $project_identifier)
+    public function setWikiPageMarkDown(WikiRequest $request, $project_identifier, $wiki_id, $name = null)
     {
-        $project = Auth::user()->projects()->where('identifier', $project_identifier)->firstOrFail();
-        $wiki = $project->wiki;
-
-        $this->validate($request, [
-            'title' => [
-                'required',
-                Rule::unique((new WikiPage())->getTable(), 'title')->where('wiki_id', $wiki->id)
-            ],
-            'text' => 'required|string'
-        ], []);
-
-        $new_page = $wiki->page()->create([
-            'title' => $request->input('title'),
-            'parent_id' => $wiki->page->first()->id
-        ]);
-
-        $new_page_content = $new_page->content()->create([
-            'author_id' => Auth::user()->id,
-            'text' => $request->input('text'),
-            'version' => 1
-        ]);
-
-        return response()->json(array_merge($new_page_content->toArray(), ['title' => $new_page->title]), 201);
+        return $this->WikiService->setWikiPageMarkDown($request, $project_identifier, $wiki_id, $name);
     }
 
-    protected function getAllWikiPage(Request $request, $project_identifier)
+    public function addNewWiki(WikiRequest $request, $project_identifier)
     {
-        $project = Auth::user()->projects()->where('identifier', $project_identifier)->firstOrFail();
-
-        $wiki = $project->wiki->page()->with('content')->get();
-
-        return $wiki;
+        return $this->WikiService->addNewWiki($request, $project_identifier);
     }
 
-    public function getNews($identifier) {
-
-        return Project::getNewsByProjectIdentifier($identifier);
-
+    public function getAllWikiPage($project_identifier)
+    {
+        return $this->WikiService->getAllWikiPage($project_identifier);
     }
+
+
+
+
 }
---
+++ app/Services/WikiService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\WikiRequest;
+use App\Models\Project;
+use App\Models\WikiPage;
+use Auth;
+
+class WikiService
+{
+    public function getWikiPageMarkDown($identifier, $page_title = null)
+    {
+        $user_projects = Auth::user()->projects;
+
+        $project = $user_projects->where('identifier', $identifier)->first();
+
+        if (is_null($project)) {
+            abort(403);
+        }
+
+        $wiki_content = $project->wiki
+            ->page()
+            ->with('content');
+
+        if ($page_title) {
+            $wiki_content->where('title', $page_title);
+        } else {
+            $wiki_content->where('parent_id', null);
+        }
+
+        $wiki_content = $wiki_content->first()->toArray();
+
+        return response()->json(array_merge(is_null($wiki_content['content']) ? [] : $wiki_content['content'] , ['title' => $wiki_content['title']]));
+    }
+
+    public function setWikiPageMarkDown(WikiRequest $request, $project_identifier, $wiki_id, $name = null)
+    {
+        $user_projects = Auth::user()->projects;
+        $project = $user_projects->where('identifier', $project_identifier)->first();
+
+        if (is_null($project)) {
+            abort(403);
+        }
+
+        $wiki_content = $project->wiki->page();
+
+        if ($name) {
+            $wiki_content->where('title', $wiki_id);
+            $wiki_id = $name;
+        } else {
+            $wiki_content->where('id', $wiki_id);
+        }
+
+        $wiki_content = $wiki_content
+            ->with(['content' => function ($q) use($wiki_id) {
+                $q->where('id', $wiki_id);
+            }])
+            ->firstOrFail();
+        $wiki_content->content->update([
+            'text' => $request->input('text')
+        ]);
+
+        $wiki_content = $wiki_content->toArray();
+
+        return response()->json(array_merge($wiki_content['content'], ['title' => $wiki_content['title']]));
+    }
+
+    public function addNewWiki(WikiRequest $request, $project_identifier)
+    {
+        $project = Auth::user()->projects()->where('identifier', $project_identifier)->firstOrFail();
+        $wiki = $project->wiki;
+
+        $new_page = $wiki->page()->create([
+            'title' => $request->input('title'),
+            'parent_id' => $wiki->page->first()->id
+        ]);
+
+        $new_page_content = $new_page->content()->create([
+            'author_id' => Auth::user()->id,
+            'text' => $request->input('text'),
+            'version' => 1
+        ]);
+
+        return response()->json(array_merge($new_page_content->toArray(), ['title' => $new_page->title]), 201);
+    }
+
+    public function getAllWikiPage($project_identifier)
+    {
+        $project = Auth::user()->projects()->where('identifier', $project_identifier)->firstOrFail();
+
+        $wiki = $project->wiki->page()->with('content')->get();
+
+        return $wiki;
+    }
+}
