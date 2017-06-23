c9ce91da3912f1a61e7abc4294cb617bd3723a6f
B@RT <bartlab@gmail.com>
2017-06-23 13:25:52 +0300

    remove tmp provider

--- resources/assets/js/services.module.js
+++ resources/assets/js/services.module.js
@@ -10,11 +10,4 @@ angular.module('app.services', [])
     .service('UsersService', UsersService)
     .service('NewsService', NewsService)
     .service('WikiService', WikiService)
-    .service('IssuesService', IssuesService)
-    .provider('IssuesServiceProvider', function () {
-        this.$get = ["IssuesService", function (IssuesService) {
-            return IssuesService;
-        }];
-
-        return this;
-    });
+    .service('IssuesService', IssuesService);
