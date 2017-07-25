da2ee9881728e69e1fd7b96035cfeaa2ba2198bc
evgeniy <flex.18.06@gmail.com>
2017-07-25 17:49:30 +0300

    add custom scrollbar to projects list

--- resources/assets/js/components/projects/list/projects-list.controller.js
+++ resources/assets/js/components/projects/list/projects-list.controller.js
@@ -3,16 +3,20 @@ import ControllerBase from 'base/controller.base';
 /**
  * @property ProjectsService
  * @property $state
+ * @property $window
  */
 export default class ProjectsListController extends ControllerBase {
 
     static get $inject() {
-        return ['ProjectsService', '$state', '$showdown'];
+        return ['ProjectsService', '$state', '$showdown', '$window'];
     }
 
     $onInit() {
         this.showClosed = 0;
         this.load();
+
+        angular.element(this.$window).bind('resize', () => this.onWindowResize());
+        this.onWindowResize();
     }
 
     load() {
@@ -32,6 +36,13 @@ export default class ProjectsListController extends ControllerBase {
 
     makeHtml(text) {
         return text ? this.$showdown.stripHtml(this.$showdown.makeHtml(text)) : '';
+    }
+
+    onWindowResize() {
+        let windowHeight = this.$window.innerHeight;
+        this.scrollBarConfig = {
+            setHeight: windowHeight - 200
+        };
     }
 
 }
