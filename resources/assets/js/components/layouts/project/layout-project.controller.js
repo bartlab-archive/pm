745f4aa7d18489a928cf7d01ddb9f85cd09c80e4
Evgeniy1806 <flex.18.06@gmail.com>
2017-08-09 17:36:50 +0300

    overview project page (project modules)

--- resources/assets/js/components/layouts/project/layout-project.controller.js
+++ resources/assets/js/components/layouts/project/layout-project.controller.js
@@ -5,27 +5,69 @@ import ControllerBase from 'base/controller.base';
  * @property $mdSidenav
  * @property $stateParams
  * @property $state
+ * @property ProjectsService
  */
 export default class LayoutProjectController extends ControllerBase {
 
     static get $inject() {
-        return ['$state', '$stateParams'];
+        return ['$state', '$stateParams', 'ProjectsService'];
     }
 
     $onInit() {
-        this.items = [
-            {url: 'projects-inner.info', name: 'Overview'},
-            {url: 'projects-inner.activity', name: 'Activity'},
-            {url: 'projects-inner.issues.index', name: 'Issues'},
-            {url: 'projects-inner.issues.gantt', name: 'Gantt'},
-            {url: 'projects-inner.issues.calendar', name: 'Calendar'},
-            {url: 'projects-inner.news', name: 'News'},
-            {url: 'projects-inner.documents', name: 'Documents'},
-            {url: 'projects-inner.wiki.index', name: 'Wiki'},
-            {url: 'projects-inner.boards', name: 'Forums'},
-            {url: 'projects-inner.files', name: 'Files'},
-            {url: 'projects-inner.settings', name: 'Settings'}
-        ];
+
+        this.ProjectsService.one(this.$stateParams.project_id).then((response) => {
+            this.items = _.get(response, 'data.enabled_modules', []);
+
+            _.forEach(this.items, (item) => {
+                switch (item.name) {
+                    case 'issue_tracking' :
+                        item.url = 'projects-inner.activity';
+                        break;
+
+                    case 'wiki' :
+                        item.url = 'projects-inner.wiki.index';
+                        break;
+
+                    case 'files' :
+                        item.url = 'projects-inner.files';
+                        break;
+
+                    case 'documents' :
+                        item.url = 'projects-inner.documents';
+                        break;
+
+                    case 'calendar' :
+                        item.url = 'projects-inner.issues.calendar';
+                        break;
+
+                    case 'gantt' :
+                        item.url = 'projects-inner.issues.gantt';
+                        break;
+
+                    default :
+                        item.url = 'projects-inner.info';
+                        break;
+                }
+            });
+
+            this.items.unshift({url: 'projects-inner.issues.index', name: 'Issues'});
+            this.items.unshift({url: 'projects-inner.info', name: 'Overview'});
+            this.items.push({url: 'projects-inner.settings', name: 'Settings'});
+        });
+
+        // this.items = [
+        //     {url: 'projects-inner.info', name: 'Overview'},
+        //     {url: 'projects-inner.activity', name: 'Activity'},
+        //     {url: 'projects-inner.issues.index', name: 'Issues'},
+        //     {url: 'projects-inner.issues.gantt', name: 'Gantt'},
+        //     {url: 'projects-inner.issues.calendar', name: 'Calendar'},
+        //     {url: 'projects-inner.news', name: 'News'},
+        //     {url: 'projects-inner.documents', name: 'Documents'},
+        //     {url: 'projects-inner.wiki.index', name: 'Wiki'},
+        //     {url: 'projects-inner.boards', name: 'Forums'},
+        //     {url: 'projects-inner.files', name: 'Files'},
+        //     {url: 'projects-inner.settings', name: 'Settings'}
+        // ];
 
         this.newItems = [
             {name: 'New issue', url: '', icon: 'create'},
