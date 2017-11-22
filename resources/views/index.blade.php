bff3326db2d2c0a4ebda7600db955ca7c7eb59b4
B@RT <bartlab@gmail.com>
2017-11-22 20:43:01 +0200

    background image, refactor index blade template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/app.css" type='text/css'>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type='text/css'>
 
     <base href="/">
     <title>PM</title>
@@ -18,6 +18,6 @@
         </div>
     </div>
 </div>
-<script src="/js/app.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
