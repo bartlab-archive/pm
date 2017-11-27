5c6ece68a1f6f57279f4f543f432ad8f91d38728
B@RT <bartlab@gmail.com>
2017-11-27 19:05:50 +0200

    footer fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,23 +10,25 @@
     <title>PM</title>
 </head>
 <body>
-<div ui-view>
-    <div class="spinner-wrapper">
-        <div class="spinner">
-            <div class="dot1"></div>
-            <div class="dot2"></div>
+<section>
+    <div ui-view>
+        <div class="spinner-wrapper">
+            <div class="spinner">
+                <div class="dot1"></div>
+                <div class="dot2"></div>
+            </div>
         </div>
     </div>
-</div>
+</section>
 
-
-<div id="footer">
+<footer>
     <div class="bgl">
         <div class="bgr">
             Powered by MaybeWorks
         </div>
     </div>
-</div>
+</footer>
+
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
