ad358ccb2c9e534aa2b824296b69493a781c36f6
B@RT <bartlab@gmail.com>
2018-07-06 11:26:32 +0300

    fix login

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,9 +17,11 @@ class UserResource extends Resource
         return [
             'id' => $this->id,
             'full_name' => $this->full_name,
+            'login' => $this->login,
             'avatar' => $this->avatar,
-            $this->mergeWhen(\Auth::user()->admin, ['request' => $this->admin]),
-            'is_group' => $this->type !== 'User',
+            $this->mergeWhen(\Auth::user() && \Auth::user()->admin, ['request' => $this->admin]),
+            $this->mergeWhen($this->type !== 'User', ['is_group' => true]),
+//            'is_group' => $this->type !== 'User',
             'created_on' => $this->created_on->format('Y-m-d H:i:s'),
             $this->mergeWhen(!empty($this->last_login_on), function () {
                 return ['last_login_on' => $this->last_login_on->format('Y-m-d H:i:s')];
